99e080f50c52efc9237f597cb6dd40d30de8000f
Nelson Alexandre Mutane <inkomomutane@github.com>
2023-04-10 17:25:24 +0200

    dashboard added

---
+++ resources/js/Layouts/partials/Sidebar.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <!-- Sidebar backdrop (mobile only) -->
+    <div class="fixed inset-0 bg-slate-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200" :class="sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'" aria-hidden="true"></div>
+
+    <!-- Sidebar -->
+    <div id="sidebar" ref="sidebar" class="flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out" :class="sidebarOpen ? 'translate-x-0' : '-translate-x-64'">
+
+      <!-- Sidebar header -->
+      <div class="flex justify-between mb-10 pr-3 sm:px-2">
+        <!-- Close button -->
+        <button ref="trigger" class="lg:hidden text-slate-500 hover:text-slate-400" @click.stop="$emit('close-sidebar')" aria-controls="sidebar" :aria-expanded="sidebarOpen">
+          <span class="sr-only">Close sidebar</span>
+          <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
+          </svg>
+        </button>
+        <!-- Logo -->
+      </div>
+
+      <!-- Links -->
+      <div class="space-y-8">
+        <!-- Pages group -->
+        <div>
+          <h3 class="text-xs uppercase text-slate-500 font-semibold pl-3">
+            <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
+            <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
+          </h3>
+          <ul class="mt-3">
+            <!-- Dashboard -->
+            <SidebarLinkGroup v-slot="parentLink" :activeCondition="true">
+              <a class="block text-slate-200 truncate transition duration-150" :class="true ? 'hover:text-slate-200' : 'hover:text-white'" href="#0" @click.prevent="sidebarExpanded ? parentLink.handleClick() : sidebarExpanded = true">
+                <div class="flex items-center justify-between">
+                  <div class="flex items-center">
+                    <svg class="shrink-0 h-6 w-6" viewBox="0 0 24 24">
+                      <path class="fill-current" :class="false ? 'text-indigo-500' : 'text-slate-400'" d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z" />
+                      <path class="fill-current" :class="true ? 'text-indigo-600' : 'text-slate-600'"  d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z" />
+                      <path class="fill-current" :class="false ? 'text-indigo-200' : 'text-slate-400'" d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z" />
+                    </svg>
+                    <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Dashboard</span>
+                  </div>
+                  <!-- Icon -->
+                  <div class="flex shrink-0 ml-2">
+                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" :class="parentLink.expanded && 'rotate-180'" viewBox="0 0 12 12">
+                      <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                    </svg>
+                  </div>
+                </div>
+              </a>
+            </SidebarLinkGroup>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Expand / collapse button -->
+      <div class="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
+        <div class="px-3 py-2">
+          <button @click.prevent="sidebarExpanded = !sidebarExpanded">
+            <span class="sr-only">Expand / collapse sidebar</span>
+            <svg class="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
+              <path class="text-slate-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
+              <path class="text-slate-600" d="M3 23H1V1h2z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted, watch } from 'vue'
+
+import SidebarLinkGroup from './SidebarLinkGroup.vue'
+
+export default {
+  name: 'Sidebar',
+  props: ['sidebarOpen'],
+  components: {
+    SidebarLinkGroup,
+  },
+  setup(props, { emit }) {
+
+    const trigger = ref(null)
+    const sidebar = ref(null)
+
+    const storedSidebarExpanded = localStorage.getItem('sidebar-expanded')
+    const sidebarExpanded = ref(storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true')
+
+
+
+    // close on click outside
+    const clickHandler = ({ target }) => {
+      if (!sidebar.value || !trigger.value) return
+      if (
+        !props.sidebarOpen ||
+        sidebar.value.contains(target) ||
+        trigger.value.contains(target)
+      ) return
+      emit('close-sidebar')
+    }
+
+    // close if the esc key is pressed
+    const keyHandler = ({ keyCode }) => {
+      if (!props.sidebarOpen || keyCode !== 27) return
+      emit('close-sidebar')
+    }
+
+    onMounted(() => {
+      document.addEventListener('click', clickHandler)
+      document.addEventListener('keydown', keyHandler)
+    })
+
+    onUnmounted(() => {
+      document.removeEventListener('click', clickHandler)
+      document.removeEventListener('keydown', keyHandler)
+    })
+
+    watch(sidebarExpanded, () => {
+      localStorage.setItem('sidebar-expanded', sidebarExpanded.value)
+      if (sidebarExpanded.value) {
+        document.querySelector('body').classList.add('sidebar-expanded')
+      } else {
+        document.querySelector('body').classList.remove('sidebar-expanded')
+      }
+    })
+
+    return {
+      trigger,
+      sidebar,
+      sidebarExpanded,
+    }
+  },
+}
+</script>
